d55e1876bce6d2a5c2a453077d614f7be602e3dc
rLaurentePn2k19 <rangie.laurente@student.passerellesnumeriques.org>
2019-11-20 18:15:56 +0800

    done

--- src/imageprocess/ImageProcess.java
+++ src/imageprocess/ImageProcess.java
@@ -1,52 +1,55 @@
 package imageprocess;
 
-
-
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 
 import javax.imageio.ImageIO;
 
-
 public class ImageProcess {
 
-   BufferedImage  image;
-   int width;
-   int height;
-   
-   public ImageProcess() {
-   
-      try {
-         File input = new File("C:\\Users\\laurentera_sd2022\\Pictures\\flower.jpg");
-         image = ImageIO.read(input);
-         width = image.getWidth();
-         height = image.getHeight();
-         
-         for(int i=0; i<height; i++) {
-         
-            for(int j=0; j<width; j++) {
-            
-               Color c = new Color(image.getRGB(j, i));
-               int red = (int)(c.getRed() * 0.299);
-               int green = (int)(c.getGreen() * 0.587);
-               int blue = (int)(c.getBlue() *0.114);
+    BufferedImage image;
+    int width;
+    int height;
+
+    public ImageProcess() {
+
+        try {
+            File input = new File("C:\\Users\\laurentera_sd2022\\Pictures\\flower.jpg");
+            image = ImageIO.read(input);
+            width = image.getWidth();
+            height = image.getHeight();
+
+            for (int i = 0; i < height; i++) {
+
+                for (int j = 0; j < width; j++) {
+
+                    Color c = new Color(image.getRGB(j, i));
+//                    int red = (int) (c.getRed() * 0.299);
+//                    int green = (int) (c.getGreen() * 0.587);
+//                    int blue = (int) (c.getBlue() * 0.114);
 //               Color newColor = new Color(red+green+blue,
-//               
 //               red+green+blue,red+green+blue);
-               Color newColor = new Color(0,0,0);
-               
-               image.setRGB(j,i,newColor.getRGB());
+                    
+                    if (c.getBlue() < 10 && c.getRed() < 10 && c.getGreen() < 10) {
+                        
+                    } else {
+                        Color white = new Color(255, 255, 255);
+                        image.setRGB(j, i, white.getRGB());
+                    }
+
+                }
             }
-         }
-         
-         File ouptut = new File("C:\\Users\\laurentera_sd2022\\Pictures\\Output.png");
-         ImageIO.write(image, "png", ouptut);
-         
-      } catch (Exception e) {}
-   }
-   
-   static public void main(String args[]) throws Exception {
-      ImageProcess obj = new ImageProcess();
-   }
+
+            File ouptut = new File("C:\\Users\\laurentera_sd2022\\Pictures\\WhiteFlower.png");
+            ImageIO.write(image, "png", ouptut);
+
+        } catch (Exception e) {
+        }
+    }
+
+    static public void main(String args[]) throws Exception {
+        ImageProcess obj = new ImageProcess();
+        System.out.println(obj.height + " " + obj.width );
+    }
 }
